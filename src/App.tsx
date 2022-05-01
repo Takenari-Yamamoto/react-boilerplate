6ae610717de7ecd047475d4a3e2a1880363221ef
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-05-02 08:06:51 +0900

    init tstodo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { InputField } from '../src/InputField';
+import { Todo } from './model';
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!todo) {
+      return;
+    }
+
+    setTodos([
+      ...todos,
+      {
+        id: Date.now(),
+        todo,
+        isDone: false,
+      },
+    ]);
+    setTodo('');
+  };
+
+  console.log(todos);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/InputField.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+export const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  return (
+    <form className="input" onSubmit={handleAdd}>
+      <input
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter"
+      />
+      <button className="submit" type="submit">
+        GO
+      </button>
+    </form>
+  );
+};
