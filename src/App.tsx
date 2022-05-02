7df639e1a24d2381408718b04e89a4666e69ae81
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-05-02 18:25:20 +0900

    一部tsに

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { decrement, increment } from './redux/counterSlice';
+import { RootState } from './redux/store';
 
 function App() {
-  const count = useSelector((state: any) => state.counter.value);
+  const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
   return (
     <div className="App">
